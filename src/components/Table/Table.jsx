38659140fb39515e890c3cf4babe3ad084a11510
smith <r.ryancedrick1993@gmail.com>
2024-02-21 23:50:02 +0800

    add: no data found in table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const Table = ({ columns, data }) => {
   return (
     <div className="overflow-x-auto rounded-tr-3xl rounded-tl-3xl mb-20 shadow-md">
-      <table className="min-w-full border bg-cyan-700 text-left">
+      <table className="min-w-full border  text-left">
         <thead>
-          <tr className="text-white">
+          <tr className="text-white bg-cyan-700">
             {columns.map((column, index) => (
               <th key={index} className=" px-4 py-4">
                 {column.header}
@@ -14,15 +14,23 @@ export const Table = ({ columns, data }) => {
           </tr>
         </thead>
         <tbody>
-          {data.map((row, rowIndex) => (
-            <tr key={rowIndex} className="even:bg-cyan-100 odd:bg-white">
-              {columns.map((column, colIndex) => (
-                <td key={colIndex} className=" px-4 py-4">
-                  {row[column.field]}
-                </td>
-              ))}
+          {data.length > 0 ? (
+            data.map((row, rowIndex) => (
+              <tr key={rowIndex} className="even:bg-cyan-100 odd:bg-white">
+                {columns.map((column, colIndex) => (
+                  <td key={colIndex} className="p-4">
+                    {row[column.field]}
+                  </td>
+                ))}
+              </tr>
+            ))
+          ) : (
+            <tr className="bg-white">
+              <td rowSpan={7} className="p-4">
+                No data found.
+              </td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
